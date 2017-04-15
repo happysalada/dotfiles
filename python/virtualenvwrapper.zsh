5acd4111b6991753c5b97237e3e13dc4743bbc2e
happysalada <raphael.megzari@gmail.com>
2017-04-15 17:46:28 +0900

    delete unused python requirements

--- python/virtualenvwrapper.zsh
+++
@@ -1,5 +0,0 @@
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Devel
-export SKLEARN_PYTHON=/Users/Raphael/Code
-source /usr/local/bin/virtualenvwrapper.sh
