c27169674e9860cd45c3311da1e0d52d825f40c5
happysalada <raphael.megzari@gmail.com>
2017-08-02 21:18:33 +0900

    cancel quit prompt from terminal

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -168,7 +168,7 @@ defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 defaults write com.apple.terminal StringEncodings -array 4
 
 # Don’t display the annoying prompt when quitting iTerm
-# defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
 ###############################################################################
 # Mac App Store                                                               #
