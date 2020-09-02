6f13b2776193976e18c628dc0102f45cafc34489
happysalada <raphael@megzari.com>
2020-09-02 19:41:22 +0900

    use home manager for ssh

--- nix/home.nix
+++ nix/home.nix
@@ -80,6 +80,44 @@
   #   };
   # };
 
+  programs.ssh = {
+    enable = true;
+    extraOptionOverrides = {
+      Include = "~/.dotfiles/ssh.config";
+    };
+    controlMaster = "auto";
+    controlPath = "~/.ssh/control/%r@%h:%p";
+    controlPersist = "10m";
+    compression = true;
+    matchBlocks."*" = {
+      extraOptions = {
+        UseRoaming = "no";
+        KexAlgorithms =
+          "curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256";
+        HostKeyAlgorithms =
+          "ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa";
+        Ciphers =
+          "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr";
+        PubkeyAuthentication = "yes";
+        MACs =
+          "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com";
+        PasswordAuthentication = "no";
+        ChallengeResponseAuthentication = "no";
+        # UseKeychain = "yes";
+        AddKeysToAgent = "yes";
+        identityFile = "~/.ssh/id_rsa";
+      };
+    };
+
+    matchBlocks.union = {
+      hostname = "136.243.134.16";
+      identityFile = "~/.ssh/hetzner";
+    };
+    matchBlocks.github = {
+      hostname = "github.com";
+      identityFile = "~/.ssh/github";
+    };
+  };
   # somehow firefox says not supported
   # programs.firefox = {
   #   enable = true;
