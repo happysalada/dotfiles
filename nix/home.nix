90a3d23d1c5df363c48a07e98374357edeb2722c
happysalada <raphael@megzari.com>
2020-10-08 12:49:44 +0900

    upgrade home-manager

--- nix/home.nix
+++ nix/home.nix
@@ -24,7 +24,7 @@ in {
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "20.03";
+  home.stateVersion = "20.09";
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
