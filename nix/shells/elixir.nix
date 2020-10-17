85925593d43e2ffc978098af8a0304a21c08adce
happysalada <raphael@megzari.com>
2020-10-17 18:50:36 +0900

    update typical elixir shell

--- nix/shells/elixir.nix
+++ nix/shells/elixir.nix
@@ -3,7 +3,7 @@ with import <nixpkgs-unstable> { };
 let
   # define packages to install with special handling for OSX
   basePackages =
-    [ git beam.packages.erlangR23.elixir_1_10 nodejs-14_x yarn postgresql_12 ];
+    [ git beam.packages.erlangR23.elixir_1_10 nodejs-14_x yarn postgresql_13 ];
 
   inputs = basePackages ++ lib.optional stdenv.isLinux inotify-tools
     ++ lib.optionals stdenv.isDarwin
@@ -12,15 +12,19 @@ let
   # define shell startup command
   hooks = ''
     # this allows mix to work on the local directory
-    mkdir -p .nix-mix
-    mkdir -p .nix-hex
+    mkdir -p $PWD/.nix-mix
+    mkdir -p $PWD/.nix-hex
     export MIX_HOME=$PWD/.nix-mix
-    export HEX_HOME=$PWD/.nix-hex
+    export HEX_HOME=$PWD/.nix-mix
     export PATH=$MIX_HOME/bin:$PATH
     export PATH=$HEX_HOME/bin:$PATH
+    mix local.hex --if-missing
     export LANG=en_US.UTF-8
     export ERL_AFLAGS="-kernel shell_history enabled"
+
+    # postges related
     export PGDATA="$PWD/db"
+
   '';
 
 in mkShell {
