417c8470660ca2e7a4eff212c98dcb9eb230bba0
happysalada <raphael@megzari.com>
2021-03-12 11:35:47 +0900

    add spacevim

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -125,6 +125,7 @@
           sha256 = "1c3rh7x8bql2m9xcn3kvdqng75lzzf6kpxb3m6knffyir0jcrfrh";
         })
         { inherit pkgs; };
+      spacevim = pkgs.spacevim.override { spacevim_config = import ./programs/spacevim.nix; };
     };
     # allow until openssl is updated
     permittedInsecurePackages = [ "openssl-1.0.2u" ];
