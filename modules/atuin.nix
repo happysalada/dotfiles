a4d22791ed2716e94f060e00fc2eb8009d28ce84
happysalada <raphael@megzari.com>
2023-09-18 17:01:38 -0400

    remove atuin sassy.technology

--- modules/atuin.nix
+++ modules/atuin.nix
@@ -1,11 +1,6 @@
-{config, ...}: {
+{ ... }: {
   services.atuin = {
     enable = true;
     # openRegistration = true;
-  };
-  services.caddy.virtualHosts."atuin.sassy.technology" = {
-    extraConfig = ''
-      reverse_proxy ${config.services.atuin.host}:${toString config.services.atuin.port}
-    '';
   };
 }
