23223665e44d93ea84a98da232f271c6b0868448
happysalada <raphael@megzari.com>
2024-11-25 02:12:20 -0500

    update lockfile

--- homes/common.nix
+++ homes/common.nix
@@ -206,7 +206,6 @@
     globalConfig = {
       tools = {
         "go:github.com/danielmiessler/fabric" = "latest";
-        "go:github.com/danielmiessler/yt" = "latest";
       };
       settings = {
         experimental = true;
